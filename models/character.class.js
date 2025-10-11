52275f6795c5cf58168c9929a860caef3b4d734a
Charrath <Marco5142@googlemail.com>
2025-10-11 13:06:17 +0200

    feat: add sharkieSwim sound

--- models/character.class.js
+++ models/character.class.js
@@ -1,35 +1,68 @@
 class Character extends MoveableObject {
   IMAGE_SETS = {
     idle: [
-      "img/1.Sharkie/1.IDLE/1.png","img/1.Sharkie/1.IDLE/2.png","img/1.Sharkie/1.IDLE/3.png",
-      "img/1.Sharkie/1.IDLE/4.png","img/1.Sharkie/1.IDLE/5.png","img/1.Sharkie/1.IDLE/6.png",
-      "img/1.Sharkie/1.IDLE/7.png","img/1.Sharkie/1.IDLE/8.png","img/1.Sharkie/1.IDLE/9.png",
-      "img/1.Sharkie/1.IDLE/10.png","img/1.Sharkie/1.IDLE/11.png","img/1.Sharkie/1.IDLE/12.png",
-      "img/1.Sharkie/1.IDLE/13.png","img/1.Sharkie/1.IDLE/14.png","img/1.Sharkie/1.IDLE/15.png",
-      "img/1.Sharkie/1.IDLE/16.png","img/1.Sharkie/1.IDLE/17.png","img/1.Sharkie/1.IDLE/18.png",
+      "img/1.Sharkie/1.IDLE/1.png",
+      "img/1.Sharkie/1.IDLE/2.png",
+      "img/1.Sharkie/1.IDLE/3.png",
+      "img/1.Sharkie/1.IDLE/4.png",
+      "img/1.Sharkie/1.IDLE/5.png",
+      "img/1.Sharkie/1.IDLE/6.png",
+      "img/1.Sharkie/1.IDLE/7.png",
+      "img/1.Sharkie/1.IDLE/8.png",
+      "img/1.Sharkie/1.IDLE/9.png",
+      "img/1.Sharkie/1.IDLE/10.png",
+      "img/1.Sharkie/1.IDLE/11.png",
+      "img/1.Sharkie/1.IDLE/12.png",
+      "img/1.Sharkie/1.IDLE/13.png",
+      "img/1.Sharkie/1.IDLE/14.png",
+      "img/1.Sharkie/1.IDLE/15.png",
+      "img/1.Sharkie/1.IDLE/16.png",
+      "img/1.Sharkie/1.IDLE/17.png",
+      "img/1.Sharkie/1.IDLE/18.png",
     ],
     longIdle: [
-      "img/1.Sharkie/2.Long_IDLE/i1.png","img/1.Sharkie/2.Long_IDLE/i2.png","img/1.Sharkie/2.Long_IDLE/i3.png",
-      "img/1.Sharkie/2.Long_IDLE/i4.png","img/1.Sharkie/2.Long_IDLE/i5.png","img/1.Sharkie/2.Long_IDLE/i6.png",
-      "img/1.Sharkie/2.Long_IDLE/i7.png","img/1.Sharkie/2.Long_IDLE/i8.png","img/1.Sharkie/2.Long_IDLE/i9.png",
-      "img/1.Sharkie/2.Long_IDLE/i10.png","img/1.Sharkie/2.Long_IDLE/i11.png","img/1.Sharkie/2.Long_IDLE/i12.png",
-      "img/1.Sharkie/2.Long_IDLE/i13.png","img/1.Sharkie/2.Long_IDLE/i14.png",
+      "img/1.Sharkie/2.Long_IDLE/i1.png",
+      "img/1.Sharkie/2.Long_IDLE/i2.png",
+      "img/1.Sharkie/2.Long_IDLE/i3.png",
+      "img/1.Sharkie/2.Long_IDLE/i4.png",
+      "img/1.Sharkie/2.Long_IDLE/i5.png",
+      "img/1.Sharkie/2.Long_IDLE/i6.png",
+      "img/1.Sharkie/2.Long_IDLE/i7.png",
+      "img/1.Sharkie/2.Long_IDLE/i8.png",
+      "img/1.Sharkie/2.Long_IDLE/i9.png",
+      "img/1.Sharkie/2.Long_IDLE/i10.png",
+      "img/1.Sharkie/2.Long_IDLE/i11.png",
+      "img/1.Sharkie/2.Long_IDLE/i12.png",
+      "img/1.Sharkie/2.Long_IDLE/i13.png",
+      "img/1.Sharkie/2.Long_IDLE/i14.png",
     ],
     swimming: [
-      "img/1.Sharkie/3.Swim/1.png","img/1.Sharkie/3.Swim/2.png","img/1.Sharkie/3.Swim/3.png",
-      "img/1.Sharkie/3.Swim/4.png","img/1.Sharkie/3.Swim/5.png","img/1.Sharkie/3.Swim/6.png",
+      "img/1.Sharkie/3.Swim/1.png",
+      "img/1.Sharkie/3.Swim/2.png",
+      "img/1.Sharkie/3.Swim/3.png",
+      "img/1.Sharkie/3.Swim/4.png",
+      "img/1.Sharkie/3.Swim/5.png",
+      "img/1.Sharkie/3.Swim/6.png",
     ],
     dead: [
-      "img/1.Sharkie/6.dead/1.Poisoned/1.png","img/1.Sharkie/6.dead/1.Poisoned/2.png",
-      "img/1.Sharkie/6.dead/1.Poisoned/3.png","img/1.Sharkie/6.dead/1.Poisoned/4.png",
-      "img/1.Sharkie/6.dead/1.Poisoned/5.png","img/1.Sharkie/6.dead/1.Poisoned/6.png",
-      "img/1.Sharkie/6.dead/1.Poisoned/7.png","img/1.Sharkie/6.dead/1.Poisoned/8.png",
-      "img/1.Sharkie/6.dead/1.Poisoned/9.png","img/1.Sharkie/6.dead/1.Poisoned/10.png",
-      "img/1.Sharkie/6.dead/1.Poisoned/11.png","img/1.Sharkie/6.dead/1.Poisoned/12.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/1.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/2.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/3.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/4.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/5.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/6.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/7.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/8.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/9.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/10.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/11.png",
+      "img/1.Sharkie/6.dead/1.Poisoned/12.png",
     ],
     hurt: [
-      "img/1.Sharkie/5.Hurt/1.Poisoned/1.png","img/1.Sharkie/5.Hurt/1.Poisoned/3.png",
-      "img/1.Sharkie/5.Hurt/1.Poisoned/5.png","img/1.Sharkie/5.Hurt/1.Poisoned/3.png",
+      "img/1.Sharkie/5.Hurt/1.Poisoned/1.png",
+      "img/1.Sharkie/5.Hurt/1.Poisoned/3.png",
+      "img/1.Sharkie/5.Hurt/1.Poisoned/5.png",
+      "img/1.Sharkie/5.Hurt/1.Poisoned/3.png",
       "img/1.Sharkie/5.Hurt/1.Poisoned/1.png",
     ],
     attackBubble: [
@@ -43,10 +76,14 @@ class Character extends MoveableObject {
       "img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png",
     ],
     attackFinalSlap: [
-      "img/1.Sharkie/4.Attack/Fin slap/1.png","img/1.Sharkie/4.Attack/Fin slap/2.png",
-      "img/1.Sharkie/4.Attack/Fin slap/3.png","img/1.Sharkie/4.Attack/Fin slap/4.png",
-      "img/1.Sharkie/4.Attack/Fin slap/5.png","img/1.Sharkie/4.Attack/Fin slap/6.png",
-      "img/1.Sharkie/4.Attack/Fin slap/7.png","img/1.Sharkie/4.Attack/Fin slap/8.png",
+      "img/1.Sharkie/4.Attack/Fin slap/1.png",
+      "img/1.Sharkie/4.Attack/Fin slap/2.png",
+      "img/1.Sharkie/4.Attack/Fin slap/3.png",
+      "img/1.Sharkie/4.Attack/Fin slap/4.png",
+      "img/1.Sharkie/4.Attack/Fin slap/5.png",
+      "img/1.Sharkie/4.Attack/Fin slap/6.png",
+      "img/1.Sharkie/4.Attack/Fin slap/7.png",
+      "img/1.Sharkie/4.Attack/Fin slap/8.png",
     ],
   };
 
@@ -72,10 +109,11 @@ class Character extends MoveableObject {
     this.loadAllImages();
     this.imagesLongIdleLast4 = this.IMAGE_SETS.longIdle.slice(-4);
     this.animate();
+    this.swimSound = new Audio("assets/audio/sharkieSwim.mp3");
   }
 
   loadAllImages() {
-    Object.values(this.IMAGE_SETS).forEach(arr => this.loadImages(arr));
+    Object.values(this.IMAGE_SETS).forEach((arr) => this.loadImages(arr));
   }
 
   animate() {
@@ -85,8 +123,10 @@ class Character extends MoveableObject {
 
   startInputLoop() {
     setInterval(() => {
-      if (this.world.keyboard.F && !this.isAttacking) this.startAttack('bubble');
-      if (this.world.keyboard.E && !this.isAttacking) this.startAttack('finalSlap');
+      if (this.world.keyboard.F && !this.isAttacking)
+        this.startAttack("bubble");
+      if (this.world.keyboard.E && !this.isAttacking)
+        this.startAttack("finalSlap");
       if (!this.isDead()) {
         if (this.canMoveRight()) this.moveRight();
         if (this.canMoveLeft()) this.moveLeft();
@@ -100,44 +140,57 @@ class Character extends MoveableObject {
   startAnimationLoop() {
     const loop = () => {
       let t = 100;
-      if (this.isDead()) { t = 150; this.handleDeadAnimation(); }
-      else if (this.attackType) { t = 40; this.handleAttackAnimation(this.attackType); }
-      else if (this.isHurt()) { t = 200; this.handleHurtAnimation(); }
-      else if (this.isMoving()) { t = 100; this.handleMovementAnimation(); }
-      else { t = 175; this.handleIdleAnimation(); }
+      if (this.isDead()) {
+        t = 150;
+        this.handleDeadAnimation();
+      } else if (this.attackType) {
+        t = 40;
+        this.handleAttackAnimation(this.attackType);
+      } else if (this.isHurt()) {
+        t = 200;
+        this.handleHurtAnimation();
+      } else if (this.isMoving()) {
+        t = 100;
+        this.handleMovementAnimation();
+      } else {
+        t = 175;
+        this.handleIdleAnimation();
+      }
       setTimeout(loop, t);
     };
     loop();
   }
 
-  updateCamera() { this.world.camera_x = -this.x + 100; }
+  updateCamera() {
+    this.world.camera_x = -this.x + 100;
+  }
 
   startAttack(type) {
-  if (this.isHurt()) return;
+    if (this.isHurt()) return;
 
-  // Wenn Bubble-Angriff versucht wird, aber keine Phiolen vorhanden sind → abbrechen
-  if (type === "bubble" && this.world.poisonBar?.number <= 0) {
-    return;
+    // Wenn Bubble-Angriff versucht wird, aber keine Phiolen vorhanden sind → abbrechen
+    if (type === "bubble" && this.world.poisonBar?.number <= 0) {
+      return;
+    }
+
+    this.attackType = type;
+    this.isAttacking = true;
+    this.currentImage = 0;
+    this.idleTimer = 0;
+
+    if (this.attackType === "bubble") {
+      // Eine Phiole verbrauchen
+      this.world.poisonBar.number -= 1;
+    }
+
+    if (this.attackType === "finalSlap") {
+      this.isUntouchable = true;
+      const attackDuration = this.IMAGE_SETS.attackFinalSlap.length * 100;
+      setTimeout(() => {
+        this.isUntouchable = false;
+      }, attackDuration + 1000);
+    }
   }
-
-  this.attackType = type;
-  this.isAttacking = true;
-  this.currentImage = 0;
-  this.idleTimer = 0;
-
-  if (this.attackType === "bubble") {
-    // Eine Phiole verbrauchen
-    this.world.poisonBar.number -= 1;
-  }
-
-  if (this.attackType === "finalSlap") {
-    this.isUntouchable = true;
-    const attackDuration = this.IMAGE_SETS.attackFinalSlap.length * 100;
-    setTimeout(() => {
-      this.isUntouchable = false;
-    }, attackDuration + 1000);
-  }
-}
 
   handleAttackAnimation(attackType) {
     if (attackType === "bubble") this.bubbleAttack();
@@ -147,7 +200,8 @@ class Character extends MoveableObject {
   finalSlapAttack() {
     const step = 4;
     if (this.currentImage < this.IMAGE_SETS.attackFinalSlap.length) {
-      this.img = this.imageCache[this.IMAGE_SETS.attackFinalSlap[this.currentImage]];
+      this.img =
+        this.imageCache[this.IMAGE_SETS.attackFinalSlap[this.currentImage]];
       if (!this.otherDirection) this.x = Math.min(this.maxX, this.x + step);
       else this.x = Math.max(this.minX, this.x - step);
       this.currentImage++;
@@ -160,7 +214,8 @@ class Character extends MoveableObject {
 
   bubbleAttack() {
     if (this.currentImage < this.IMAGE_SETS.attackBubble.length) {
-      this.img = this.imageCache[this.IMAGE_SETS.attackBubble[this.currentImage]];
+      this.img =
+        this.imageCache[this.IMAGE_SETS.attackBubble[this.currentImage]];
       this.currentImage++;
     } else {
       this.spawnBubble();
@@ -202,6 +257,10 @@ class Character extends MoveableObject {
     this.playAnimation(this.IMAGE_SETS.swimming);
     this.resetIdle();
     this.longIdlePlayed = false;
+    if (this.swimSound && this.swimSound.paused) {
+      this.swimSound.currentTime = 0;
+      this.swimSound.play();
+    }
   }
 
   handleIdleAnimation() {
@@ -226,19 +285,24 @@ class Character extends MoveableObject {
       this.animationState = "longIdleLoop";
       this.currentImage = 0;
       Object.assign(this.offset, { top: 132, bottom: 62 });
-
     }
   }
 
-  resetIdle() { 
-    this.idleTimer = 0; 
-    this.animationState = "idle"; 
+  resetIdle() {
+    this.idleTimer = 0;
+    this.animationState = "idle";
     Object.assign(this.offset, { top: 120, bottom: 85 });
   }
 
-  moveRight() { this.x += this.speed; this.otherDirection = false; }
-  
-  moveLeft() { this.x -= this.speed; this.otherDirection = true; }
+  moveRight() {
+    this.x += this.speed;
+    this.otherDirection = false;
+  }
+
+  moveLeft() {
+    this.x -= this.speed;
+    this.otherDirection = true;
+  }
 
   playDeadAnimation() {
     if (this.deadAnimationIndex < this.IMAGE_SETS.dead.length) {
