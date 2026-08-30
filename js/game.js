8a8aec4ef844818244f1974ec3d11b9908e772f3
Charrath <Marco5142@googlemail.com>
2026-08-30 20:09:01 +0200

    add start-screen

--- js/game.js
+++ js/game.js
@@ -3,52 +3,54 @@ let world;
 let keyboard = new Keyboard();
 
 function init() {
-  canvas = document.getElementById('canvas');
-  world = new World(canvas, keyboard);
-
-  console.log("My Character is", world.character);
+  canvas = document.getElementById("canvas");
 }
 
-document.addEventListener('keydown', (event) => {
-  if(event.key === "ArrowLeft" || event.code === "KeyA") {
+function startGame() {
+  document.getElementById("startScreen").style.display = "none";
+  world = new World(canvas, keyboard);
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft" || event.code === "KeyA") {
     keyboard.LEFT = true;
   }
-  if(event.key === "ArrowUp" || event.code === "KeyW") {
+  if (event.key === "ArrowUp" || event.code === "KeyW") {
     keyboard.UP = true;
   }
-  if(event.key === "ArrowRight" || event.code === "KeyD") {
+  if (event.key === "ArrowRight" || event.code === "KeyD") {
     keyboard.RIGHT = true;
   }
-  if(event.key === "ArrowDown" || event.code === "KeyS") {
+  if (event.key === "ArrowDown" || event.code === "KeyS") {
     keyboard.DOWN = true;
   }
-  if(event.code === "KeyF") {
+  if (event.code === "KeyF") {
     keyboard.F = true;
   }
-  if(event.code === "KeyE") {
+  if (event.code === "KeyE") {
     keyboard.E = true;
   }
   console.log(event);
 });
 
-document.addEventListener('keyup', (event) => {
-  if(event.key === "ArrowLeft" || event.code === "KeyA") {
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowLeft" || event.code === "KeyA") {
     keyboard.LEFT = false;
   }
-  if(event.key === "ArrowUp" || event.code === "KeyW") {
+  if (event.key === "ArrowUp" || event.code === "KeyW") {
     keyboard.UP = false;
   }
-  if(event.key === "ArrowRight" || event.code === "KeyD") {
+  if (event.key === "ArrowRight" || event.code === "KeyD") {
     keyboard.RIGHT = false;
   }
-  if(event.key === "ArrowDown" || event.code === "KeyS") {
+  if (event.key === "ArrowDown" || event.code === "KeyS") {
     keyboard.DOWN = false;
   }
-  if(event.code === "KeyF") {
+  if (event.code === "KeyF") {
     keyboard.F = false;
   }
-  if(event.code === "KeyE") {
+  if (event.code === "KeyE") {
     keyboard.E = false;
   }
-console.log(event);
+  console.log(event);
 });
